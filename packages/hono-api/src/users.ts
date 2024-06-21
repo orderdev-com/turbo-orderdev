37355b0162ff9b1a7f6120191269e075066e23fb
kerem <keremtiryaki@gmail.com>
2024-06-21 17:27:24 +0300

    remove openapi, not needed now

--- packages/hono-api/src/users.ts
+++ packages/hono-api/src/users.ts
@@ -1,84 +1,15 @@
 import { Hono } from 'hono'
-import { OpenAPIHono, z, createRoute } from '@hono/zod-openapi'
 import { zValidator } from '@hono/zod-validator'
-import { swaggerUI } from '@hono/swagger-ui'
-// import { z } from 'zod'
+import { z } from 'zod'
 
 // User schema
 const UserSchema = z.object({
     id: z
         .string()
-        .min(3)
-        .openapi({
-            param: {
-                name: 'id',
-                in: 'path',
-            },
-            example: '1001',
-        }),
+        .min(3),
     name: z.string(),
     email: z.string().email(),
 })
-
-const ParamsSchema = z.object({
-    id: z
-        .string()
-        .min(3)
-        .openapi({
-            param: {
-                name: 'id',
-                in: 'path',
-            },
-            example: '1001',
-        }),
-})
-
-
-
-const route = createRoute({
-    method: 'get',
-    path: '/api/users/{id}',
-    request: {
-        params: ParamsSchema,
-    },
-    responses: {
-        200: {
-            content: {
-                'application/json': {
-                    schema: UserSchema,
-                },
-            },
-            description: 'Retrieve the user',
-        },
-    },
-})
-
-export const openapiApp = new OpenAPIHono()
-
-// openapiApp.openapi(route, (c) => {
-//     const { id } = c.req.valid('param')
-//     return c.json(
-//         {
-//             id,
-//             name: 'Ultra-man',
-//             email: 'ultra@example.com',
-//         },
-//         200 // You should specify the status code even if it is 200.
-//     )
-// })
-
-// The OpenAPI documentation will be available at /doc
-openapiApp.doc('/doc', {
-    openapi: '3.0.0',
-    info: {
-        version: '1.0.0',
-        title: 'My API',
-    },
-})
-
-// Use the middleware to serve Swagger UI at /ui
-openapiApp.get('/ui', swaggerUI({ url: '/api/openapi/doc' }))
-
 
 type User = z.infer<typeof UserSchema>
 
